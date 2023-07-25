78909e16b232ede2b6179cd035d5758f34565def
EskurinhoES <eskurinhoes082023@gmail.com>
2023-07-25 15:09:05 -0300

    Revisão de javascript básico

---
+++ projeto/index.js
@@ -0,0 +1,20 @@
+/*Projeto de testes com o aprendizado básico
+de javascript*/
+let a = 10;
+const b = '10';
+console.log("Olá Mundo!!!");
+console.log("Hello World!!!");
+console.log(a !== b);
+
+let cor = "verde";
+
+if (cor === "verde"){
+
+    console.log("SIGA");
+
+} else{
+
+    console.log("PARE");
+
+}
+document.write("Hello World!!!");
---
+++ projeto/index2.js
@@ -0,0 +1,22 @@
+let cor = "amerelo";
+
+switch (cor) {
+    case "verde":
+        console.log("SIGA");
+        alert("SIGA");
+        break;
+
+    case "amarelo":
+        console.log("ATENÇÃO");
+        alert("ATENÇÂO");
+        break;
+
+    case "vermelho":
+        console.log("PARE");
+        alert("PARE");
+        break;
+
+    default:
+        alert("COR INVÁLIDA");
+        console.log("COR INVÁLIDA");
+}
